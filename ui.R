30a43216acc3bbe3780b72ce374da9e5f2ccbc1c
Armann Atli Sigurðsson <armannsig@gmail.com>
2015-09-30 19:36:58 +0000

    Correction

--- ui.R
+++ ui.R
@@ -5,7 +5,6 @@ library(shinyAce)
 
 # All UI elements must be inside shinyUI()
 shinyUI(
-<<<<<<< HEAD
   
   pageWithSidebar(
     headerPanel("Immigrants in Iceland"),
@@ -108,97 +107,4 @@ shinyUI(
       )
     )
   )
-=======
-
-pageWithSidebar(
-headerPanel("Immigrants in Iceland"),
-
-sidebarPanel(
-tabsetPanel(
-tabPanel("Editor",
-# Code editor window.
-aceEditor("plotCode",
-mode = "r",
-value = "ggplot(data = cars,\n    aes(x = speed, y = dist)) + \n    geom_point(col = 'red')")
-
-# downloadButton(
-#  outputId = "downloadCode",
-# label    = "Dowgnload Code")
-))),
-
-
-
-mainPanel(
-tabsetPanel(
-
-tabPanel("Data Manager",
-# User uploads data file here.
-fileInput(
-inputId = "uploadFile",
-label   = "Upload a CSV data file. You may upload multiple files and use them all in your plot, but you must upload them one at a time. Note: If you upload a file with a name that's already being used, the app will append a numeric suffix to ensure no data is overwritten."),
-
-h5("Data available:"),
-
-# This is returned once a valid data.frame name is entered
-# above. It allows the user to pick an x-variable and y-variable
-# from the column names of the selected data.frame.
-uiOutput(outputId = "dataInfo")
-),
-
-tabPanel("Plot",
-# Show the plot itself.
-plotOutput(outputId = "plot"),
-
-# Button to update plot.
-actionButton("plotButton", "Update Plot")
-),
-
-tabPanel("Download",
-
-# Allow the user to choose the download file type.
-selectInput(
-inputId = "downloadPlotType",
-label   = h5("Select download file type"),
-choices = list(
-"PDF"  = "pdf",
-"BMP"  = "bmp",
-"JPEG" = "jpeg",
-"PNG"  = "png")),
-
-# Allow the user to set the height and width of the plot download.
-h5(HTML("Set download image dimensions<br>(units are inches for PDF, pixels for all other formats)")),
-
-numericInput(
-inputId = "downloadPlotHeight",
-label = "Height (inches)",
-value = 7,
-min = 1,
-max = 100),
-
-numericInput(
-inputId = "downloadPlotWidth",
-label = "Width (inches)",
-value = 7,
-min = 1,
-max = 100),
-
-# Choose download filename.
-textInput(
-inputId = "downloadPlotFileName",
-label = h5("Enter file name for download")),
-
-div(),
-
-# File downloads when this button is clicked.
-downloadButton(
-outputId = "downloadPlot",
-label    = "Download Plot")
-)
-
-
-
-)
-)
-)
->>>>>>> origin/master
 )
